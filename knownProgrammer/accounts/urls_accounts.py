4953bdd4c8745796d213ced36e37e5772d1c1421
kuba <jakubpolec@outlook.com>
2024-04-06 15:26:42 +0200

    Commit 35 | Feat Register_Funcionality

--- knownProgrammer/accounts/urls_accounts.py
+++ knownProgrammer/accounts/urls_accounts.py
@@ -4,11 +4,14 @@ from django.urls import path
 from accounts.views import (
     login_user,
     logout_user,
+    register_user,
+
 )
 
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('register/', register_user, name='register'),
     path('reset_password/', views.PasswordResetView.as_view(template_name="accounts/reset_password.html"),
          name="reset_password"),
     path('reset_password_done/', views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
