061b5296568e65e7f90a85965a264c7bcb37229c
OktawianCzakiert <oktawian.czakiert@gmail.com>
2024-04-07 11:30:05 +0200

    1. Modified 5-star rating so the images are uploaded from static/images folder.

--- knownProgrammer/programmers/templatetags/stars.py
+++ knownProgrammer/programmers/templatetags/stars.py
@@ -1,4 +1,5 @@
 from django import template
+from django.templatetags.static import static
 
 register = template.Library()
 
@@ -10,13 +11,21 @@ def int_range(value):
 
 @register.filter
 def get_rating_stars(value):
-    rating_float = float(value)  # Convert to float for more precise calculations
-    full_stars = int(rating_float)  # Number of full stars
-    half_stars = round((rating_float - full_stars) * 2)  # Number of half stars (rounded)
-    empty_stars = 5 - full_stars - half_stars  # Number of empty stars
+    rating_float = float(value)
+    full_stars = int(rating_float)
+    half_stars = round((rating_float - full_stars) * 2)
+    empty_stars = 5 - full_stars - half_stars
+    stars = []
 
-    stars = ["⭐" for _ in range(full_stars)]  # Add full stars
-    if half_stars == 1:  # Add half star if needed
-        stars.append("½⭐")
-    stars.extend(["☆" for _ in range(empty_stars)])  # Add empty stars
-    return stars
+    for _ in range(full_stars):
+        stars.append(f'<img width="30" height="30" src="{static("images/star-full.png")}" alt="star-full">')
+
+        # Add half star if needed
+    if half_stars == 1:
+        stars.append(f'<img width="30" height="30" src="{static("images/star-half.png")}" alt="star-half-empty">')
+
+        # Add empty stars
+    for _ in range(empty_stars):
+        stars.append(f'<img width="30" height="30" src="{static("images/star-empty.png")}" alt="star-empty">')
+
+    return ''.join(stars)
