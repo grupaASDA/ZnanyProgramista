920b0e5c8cbf84cbd536be7411cf4c8dd94a3906
kuba <jakubpolec@outlook.com>
2024-04-13 14:19:25 +0200

    Fixed communication app

--- knownProgrammer/communication/urls_communication.py
+++ knownProgrammer/communication/urls_communication.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 from communication.views import (
     send_message_view,
-    my_messages_person_list_view,
-    my_messages_with_user_view,
+    my_messages_people_list_view,
+    my_messages_with_correspondent_view,
     message_view,
     replay_message_view,
 )
 
 urlpatterns = [
     path('send/<int:id>', send_message_view, name='send_message'),
-    path('contacts_list/', my_messages_person_list_view, name='messages_person_list'),
-    path('messages_list/<int:id>/', my_messages_with_user_view, name='messages_with_list'),
+    path('contacts_list/', my_messages_people_list_view, name='messages_person_list'),
+    path('messages_list/<int:id>/', my_messages_with_correspondent_view, name='messages_with_list'),
     path('message/<int:id>', message_view, name='message'),
     path('replay/<int:id>', replay_message_view, name='replay')
 ]
--- knownProgrammer/communication/views.py
+++ knownProgrammer/communication/views.py
@@ -13,6 +13,20 @@ login_redirect = reverse_lazy("login")
 
 # Create your views here.
 
+def get_last_message(user_id):
+    last_message_i_sent_date = Message.objects.filter(sent_by=user_id).order_by('-created_at').values('created_at').first()
+    last_message_i_get_date = Message.objects.filter(sent_to=user_id).order_by('-created_at').values('created_at').first()
+    last_message_i_sent = Message.objects.filter(sent_by=user_id).order_by('-created_at').first()
+    last_message_i_get = Message.objects.filter(sent_to=user_id).order_by('-created_at').first()
+    if last_message_i_sent_date and last_message_i_get_date:
+        if last_message_i_sent_date['created_at'] > last_message_i_get_date['created_at']:
+            return last_message_i_sent
+        return last_message_i_get
+    elif last_message_i_sent_date:
+        return last_message_i_sent
+    elif last_message_i_get_date:
+        return last_message_i_get
+
 @login_required(login_url=login_redirect)
 def send_message_view(request, id):
     user_profile_exists = CustomUser.objects.filter(id=id).exists()
@@ -101,31 +115,46 @@ def replay_message_view(request, id):
         return render(request, template_name="communication/replay_message.html", context=ctx)
 
 @login_required(login_url=login_redirect)
-def my_messages_person_list_view(request):
-    people_i_sent_message = Message.objects.filter(sent_by=request.user.id).values('sent_to').distinct()
-    people_who_sent_message = Message.objects.filter(sent_to=request.user.id).values('sent_by').distinct()
+def my_messages_people_list_view(request):
+    recipients_ids = Message.objects.filter(sent_by=request.user.id).values('sent_to').distinct()
+    senders_ids = Message.objects.filter(sent_to=request.user.id).values('sent_by').distinct()
+
+    recipients = {user_id['sent_to']: get_object_or_404(CustomUser, id=user_id['sent_to']) for user_id in recipients_ids}
+    senders = {user_id['sent_by']: get_object_or_404(CustomUser, id=user_id['sent_by']) for user_id in senders_ids}
+
+    contacts = []
+
+    for contact in recipients.values():
+        contacts.append(contact)
+
+    for contact_id, contact in senders.items():
+        if contact_id not in recipients.keys():
+            contacts.append(contact)
+
+    for index, contact in enumerate(contacts):
+        contacts[index] = [contact, get_last_message(contact.id)]
+
+    contacts.sort(key=lambda x: x[1].created_at, reverse=True)
 
 
-    people_i_sent_message_info = [get_object_or_404(CustomUser, id=user_id['sent_to']) for user_id in people_i_sent_message]
-    people_who_sent_message_info = [get_object_or_404(CustomUser, id=user_id['sent_by']) for user_id in people_who_sent_message]
 
     if request.method == 'GET':
         ctx = {
-            'people_i_sent_message': people_i_sent_message_info,
-            'people_who_sent_message': people_who_sent_message_info,
+            'contacts': contacts,
         }
         return render(request, 'communication/my_messages_person_list.html', context=ctx)
 
 @login_required(login_url=login_redirect)
-def my_messages_with_user_view(request, id):
+def my_messages_with_correspondent_view(request, id):
     user = get_object_or_404(CustomUser, id=id)
-    messages_sent_by_me = Message.objects.filter(sent_by=request.user.id, sent_to=id).order_by('-created_at')
-    messages_sent_to_me = Message.objects.filter(sent_by=id, sent_to=request.user.id).order_by('-created_at')
+    sent_by_me = Message.objects.filter(sent_by=request.user.id, sent_to=id).order_by('-created_at')
+    sent_to_me = Message.objects.filter(sent_by=id, sent_to=request.user.id).order_by('-created_at')
+
     if request.method == 'GET':
         ctx = {
             'user': user,
-            'messages_sent_by_me': messages_sent_by_me,
-            'messages_sent_to_me': messages_sent_to_me,
+            'sent_by_me': sent_by_me,
+            'sent_to_me': sent_to_me,
         }
         return render(request, 'communication/my_messages_with_correspondent.html', context=ctx)
 
